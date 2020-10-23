1b6b1dff1ba582dda4756ab2e0d42fe8145311db
Joe Mazzone <jmazzone@daviestech.org>
2020-10-23 03:08:51 +0000

    Initial commit

---
+++ Lesson2_3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Lesson2_3{
+  
+  public static void main (String[] args) {
+
+    //Create a String Object. String is the class
+    
+    //Create a Scanner Object. Scanner is the class
+   
+   
+  }
+
+}
+
+
