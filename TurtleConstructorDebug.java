d4d919dcf52ca75fad8d80db74291e032483891e
Joe Mazzone <jmazzone@daviestech.org>
2020-10-30 15:51:13 +0000

    update

---
+++ TurtleConstructorDebug.java
@@ -0,0 +1,15 @@
+
+public class TurtleConstructorDebug {
+  public static void main(String[] args)   {
+      
+      World w = new World(300,0);
+      turtle t0;
+      Turtle t1 = new Turtle();
+      Turtle t2 = new Turtle(world, 100, 50)
+      t0.forward();
+      t1.turnRight();
+      t2.turnLeft();
+      world.show(true);
+      
+  }
+}
--- TurtleDrawing.java
+++ TurtleDrawing.java
@@ -4,7 +4,7 @@ class TurtleDrawing {
   
   public static void main (String[] args) {
 
-    
+      
    
    
   }
